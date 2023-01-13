6695c36fa3cc7970fadb91133fbbab74b98a1a83
Jānis Alunāns <j.alunans@gmail.com>
2023-01-13 12:22:40 +0200

    Add total to Cart view

--- src/Cart.js
+++ src/Cart.js
@@ -1,12 +1,23 @@
 import CartItem from './CartItem'
 
 function Cart({ cart, remove, incrementBy }) {
+
+  function total () {
+    return cart.reduce((acc,item)=>{
+      return acc + (item.product.price * item.amount)
+    }, 0)
+  }
+
   return (
     <div className="cart">
       <h1>Cart</h1>
       {cart.map(item=>
         <CartItem key={item.id} product={item.product} amount={item.amount} remove={remove} incrementBy={incrementBy} />
       )}
+
+      <div className="total">
+        <p>Total to pay: {total()}</p>
+      </div>
     </div>
   )
 }
